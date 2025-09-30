3de15b21113ed743aa020c6091f2749482ea4b3d
LiyaDyakov <dyakovliya23@gmail.com>
2025-09-30 12:23:26 -0400

    Connect the database to the Flaskr-tdd project

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 #app.py
 import sqlite3
 from pathlib import Path
+import os
 
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
@@ -18,6 +19,12 @@ SECRET_KEY = "change_me"
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 
 # create and initialize a new Flask app
 app = Flask(__name__)
